1be89a1a0492ee7ca2d493324f8f5cfbe4c5b12a
Karoly Molnar <molnarkares@gmail.com>
2025-08-12 20:29:02 +0200

    WIP some graphics

--- drivers/display/display_gu_vfd.c
+++ drivers/display/display_gu_vfd.c
@@ -39,11 +39,24 @@ union gu_vfd_bus {
 };
 
 typedef bool (*gu_vfd_connection_ready_fn)(const struct device *dev);
+
 typedef int (*gu_vfd_write_fn)(const struct device *dev, uint8_t data);
-typedef int (*gu_vfd_read_fn)(const struct device *dev, uint8_t * data);
+
+typedef int (*gu_vfd_read_fn)(const struct device *dev, uint8_t *data);
+
 typedef const char *(*gu_vfd_connection_name_fn)(const struct device *dev);
 
-static int gu_vfd_write_buffer(const struct device *dev, uint8_t *const buf, const size_t len, bool packet);
+static int gu_vfd_write_buffer(const struct device *dev, uint8_t *buf, size_t len);
+
+static int gu_vfd_binary_mode(const struct device *dev);
+
+static int gu_vfd_graphic_write(const struct device *dev, uint8_t data[], uint8_t len);
+
+static int gu_vfd_vertical_mode(const struct device *dev);
+
+static int gu_vfd_set_brightness(const struct device *dev, uint8_t brightness);
+
+static int gu_vfd_set_pos(const struct device *dev, uint8_t x, uint8_t y);
 
 struct gu_vfd_data {
     uint8_t *dummy_buffer;
@@ -62,42 +75,47 @@ struct gu_vfd_config {
     uint16_t width;
 };
 
-static int gu_vfd_wait_for_ready(const struct device *dev)
-{
+
+static int gu_vfd_wait_for_ready(const struct device *dev) {
     int mb_state = 0;
     const struct gu_vfd_config *config = dev->config;
     do {
         mb_state = gpio_pin_get_dt(&config->mb_pin);
-    }while (mb_state == 1);
+    } while (mb_state == 1);
     return 0;
 }
 
 static int gu_vfd_write(const struct device *dev, const uint16_t x,
-                           const uint16_t y,
-                           const struct display_buffer_descriptor *desc,
-                           const void *buf)
-{
+                        const uint16_t y,
+                        const struct display_buffer_descriptor *desc,
+                        const void *buf) {
+    int ret = 0;
+    if (x > desc->width || y > desc->height) {
+        return -EINVAL;
+    }
 
+    ret |= gu_vfd_set_pos(dev, (uint8_t)x, (uint8_t)y);
 
-    return 0;
+    uint8_t gfx[] = {0x1c, 0x5c, 0x48, 0x3e, 0x1d, 0x1d, 0x14, 0x36};
+
+    ret |= gu_vfd_graphic_write(dev, gfx, sizeof(gfx));
+
+    return ret;
 }
 
-static int gu_vfd_blanking_on(const struct device *dev)
-{
-    uint8_t cmd[] = { GU_VFD_CMD_POWER_OFF>>8, GU_VFD_CMD_POWER_OFF&0xFFu };
-    return gu_vfd_write_buffer(dev,cmd,sizeof(cmd),false);
+static int gu_vfd_blanking_on(const struct device *dev) {
+    uint8_t cmd[] = {GU_VFD_CMD_POWER_OFF >> 8, GU_VFD_CMD_POWER_OFF & 0xFFu};
+    return gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
 }
 
-static int gu_vfd_blanking_off(const struct device *dev)
-{
-    uint8_t cmd[] = { GU_VFD_CMD_POWER_ON>>8, GU_VFD_CMD_POWER_ON&0xFFu };
-    return gu_vfd_write_buffer(dev,cmd,sizeof(cmd), false);
+static int gu_vfd_blanking_off(const struct device *dev) {
+    uint8_t cmd[] = {GU_VFD_CMD_POWER_ON >> 8, GU_VFD_CMD_POWER_ON & 0xFFu};
+    return gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
 }
 
 
 static void gu_vfd_get_capabilities(const struct device *dev,
-                                       struct display_capabilities *cap)
-{
+                                    struct display_capabilities *cap) {
     memset(cap, 0, sizeof(struct display_capabilities));
     const struct gu_vfd_config *config = dev->config;
     cap->x_resolution = config->width;
@@ -109,27 +127,23 @@ static void gu_vfd_get_capabilities(const struct device *dev,
 
 /* --- Init --- */
 static int gu_vfd_init(const struct device *dev) {
+    int ret = 0;
     const struct gu_vfd_config *config = dev->config;
-    if (!config->connection_ready(dev))
-    {
+    if (!config->connection_ready(dev)) {
         LOG_ERR("Serial connection device is not ready");
         return -ENODEV;
     }
 
 
     /* Reset if pin connected then perform physical Reset */
-    if (config->reset_pin.port)
-    {
-
-        if (!gpio_is_ready_dt(&config->reset_pin))
-        {
+    if (config->reset_pin.port) {
+        if (!gpio_is_ready_dt(&config->reset_pin)) {
             LOG_ERR("RESET GPIO device not ready");
             return -ENODEV;
         }
 
-        const int ret = gpio_pin_configure_dt(&config->reset_pin, GPIO_OUTPUT_INACTIVE);
-        if (ret < 0)
-        {
+        ret = gpio_pin_configure_dt(&config->reset_pin, GPIO_OUTPUT_INACTIVE);
+        if (ret < 0) {
             LOG_ERR("Unable to set RESET pin to OUTPUT");
             return ret;
         }
@@ -142,15 +156,13 @@ static int gu_vfd_init(const struct device *dev) {
     }
 
 
-    const int ret = gpio_pin_configure_dt(&config->mb_pin, GPIO_INPUT);
-    if (ret < 0)
-    {
+    ret = gpio_pin_configure_dt(&config->mb_pin, GPIO_INPUT);
+    if (ret < 0) {
         LOG_ERR("Unable to set MB pin to INPUT");
         return ret;
     }
 
-    if (!gpio_is_ready_dt(&config->mb_pin))
-    {
+    if (!gpio_is_ready_dt(&config->mb_pin)) {
         LOG_ERR("MB GPIO device not ready");
         return -ENODEV;
     }
@@ -160,101 +172,90 @@ static int gu_vfd_init(const struct device *dev) {
 
     gu_vfd_wait_for_ready(dev);
     const int mb_state = gpio_pin_get_dt(&config->mb_pin);
-    if (mb_state != 0)
-    {
+    if (mb_state != 0) {
         LOG_ERR("MB GPIO pin shall be LOW after init");
         return -ENODEV;
     }
 
-    // uint8_t comm_setup[] = {GU_VFD_CMD_SET_COMM>>8, GU_VFD_CMD_SET_COMM& 0xFFu,
-    // GU_VFD_SERIAL_PACKET_MODE_ON|GU_VFD_SERIAL_COMM_BUFFER_ON};
-    // gu_vfd_write_buffer(dev,comm_setup,sizeof(comm_setup), false);
+    ret = gu_vfd_binary_mode(dev);
+    ret |= gu_vfd_vertical_mode(dev);
+    ret |= gu_vfd_set_brightness(dev,CONFIG_GU_VFD_DEFAULT_BRIGHTNESS);
 
-    /* disable HEX mode */
-    uint8_t cmd[] = {GU_VFD_CMD_BINARY_MODE >> 8, GU_VFD_CMD_BINARY_MODE& 0xFFu};
-    gu_vfd_write_buffer(dev,cmd,sizeof(cmd), false);
-
-
-    uint8_t message[] = {'M','y',' ','t','e','s','t'};
-    gu_vfd_write_buffer(dev,message,sizeof(message), false);
-
-
-    return 0;
+    return ret;
 }
 
-static int gu_vfd_deinit(const struct device *dev)
-{
+static int gu_vfd_deinit(const struct device *dev) {
     /* wait for finalizing current operation */
     gu_vfd_wait_for_ready(dev);
     return 0;
 }
 
-static int gu_vfd_set_brightness(const struct device *dev, const uint8_t brightness)
-{
+static int gu_vfd_vertical_mode(const struct device *dev) {
+    uint8_t cmd[2] = {GU_VFD_CMD_WRITE_MODE, GU_VFD_WRITE_MODE_GFX_VERTICAL};
+    return gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
+}
+
+static int gu_vfd_graphic_write(const struct device *dev, uint8_t data[], uint8_t len) {
     int ret = 0;
-    if (brightness > 8)
-    {
+    uint8_t cmd[] = {GU_VFD_CMD_GRAPHIC_WRITE, len};
+    ret = gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
+    ret |= gu_vfd_write_buffer(dev, data, len);
+    return ret;
+}
+
+static int gu_vfd_set_brightness(const struct device *dev, const uint8_t brightness) {
+    int ret = 0;
+    if (brightness > 8) {
         ret = -EINVAL;
-    }
-    else
-    {
-        uint8_t cmd[] = { GU_VFD_CMD_BRIGHTNESS, GU_VFD_BRIGHTNESS_0 + brightness };
-        ret = gu_vfd_write_buffer(dev,cmd,sizeof(cmd), false);
+    } else {
+        uint8_t cmd[] = {GU_VFD_CMD_BRIGHTNESS, GU_VFD_BRIGHTNESS_0 + brightness};
+        ret = gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
     }
 
     return ret;
 }
 
-static int gu_vfd_clear(const struct device *dev)
-{
+static int gu_vfd_clear(const struct device *dev) {
     const struct gu_vfd_config *config = dev->config;
-    uint8_t cmd[] = { GU_VFD_CMD_CLEAR_AREA, 0, 0, config->width,config->height };
-    return gu_vfd_write_buffer(dev,cmd,sizeof(cmd), false);
+    uint8_t cmd[] = {GU_VFD_CMD_CLEAR_AREA, 0, 0, config->width, config->height};
+    return gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
+}
 
+static int gu_vfd_binary_mode(const struct device *dev) {
+    uint8_t cmd[2] = {GU_VFD_CMD_BINARY_MODE >> 8,GU_VFD_CMD_BINARY_MODE & 0xFF};
+    return gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
 }
 
 
+static int gu_vfd_set_pos(const struct device *dev, uint8_t x, uint8_t y) {
+    uint8_t cmd[] = {GU_VFD_CMD_CURSOR_POS, x, y};
+    return gu_vfd_write_buffer(dev, cmd, sizeof(cmd));
+}
+
 #if (DT_HAS_COMPAT_ON_BUS_STATUS_OKAY(DT_DRV_COMPAT, spi))
-static bool gu_vfd_connection_ready_spi(const struct device *dev)
-{
+static bool gu_vfd_connection_ready_spi(const struct device *dev) {
     const struct gu_vfd_config *config = dev->config;
 
     return spi_is_ready_dt(&config->connection.spi);
 }
 
-static int gu_vfd_write_buffer(const struct device *dev, uint8_t *const buf, const size_t len, bool packet)
-{
+static int gu_vfd_write_buffer(const struct device *dev, uint8_t *const buf, const size_t len) {
     const struct gu_vfd_config *config = dev->config;
-    if (packet == true)
-    {
-        uint8_t cmd[] = {GU_VFD_CMD_PACKET_START, len};
-        gu_vfd_write_buffer(dev,cmd,sizeof(cmd), false);
-    }
-    uint8_t checksum = 0;
-    for (size_t i = 0; i < len; i++)
-    {
+    for (size_t i = 0; i < len; i++) {
         uint8_t data = buf[i];
-        checksum += data;
-        config->write(dev,data);
+        config->write(dev, data);
         gu_vfd_wait_for_ready(dev);
-    }
-    if (packet == true)
-    {
-        uint8_t cmd[] = {checksum, GU_VFD_CMD_PACKET_STOP};
-        gu_vfd_write_buffer(dev,cmd,sizeof(cmd), false);
     }
     return 0;
 }
 
-static int gu_vfd_write_spi(const struct device *dev, uint8_t const data)
-{
-
+static int gu_vfd_write_spi(const struct device *dev, uint8_t const data) {
     const struct gu_vfd_config *config = dev->config;
 
-    uint8_t dummy_rx =0;
+    uint8_t dummy_rx = 0;
 
     struct spi_buf tx_buf = {
-        .buf = (uint8_t*)&data,
+        .buf = (uint8_t *) &data,
         .len = 1
     };
 
@@ -275,8 +276,7 @@ static int gu_vfd_write_spi(const struct device *dev, uint8_t const data)
     return spi_transceive_dt(&config->connection.spi, &tx_bufs, &rx_bufs);
 }
 
-static int gu_vfd_read_spi(const struct device *dev, uint8_t * data)
-{
+static int gu_vfd_read_spi(const struct device *dev, uint8_t *data) {
     const struct gu_vfd_config *config = dev->config;
     uint8_t dummy_tx = GU_VFD_CMD_DUMMY;
 
@@ -304,8 +304,7 @@ static int gu_vfd_read_spi(const struct device *dev, uint8_t * data)
     return spi_transceive_dt(&config->connection.spi, &tx_bufs, &rx_bufs);
 }
 
-static const char *gu_vfd_connection_name_spi(const struct device *dev)
-{
+static const char *gu_vfd_connection_name_spi(const struct device *dev) {
     const struct gu_vfd_config *config = dev->config;
 
     return config->connection.spi.bus->name;
@@ -351,7 +350,6 @@ static DEVICE_API(display, gu_vfd_driver_api) = {
 	};                                                                              \
                                                                                     \
 	DEVICE_DT_DEINIT_DEFINE(node_id, gu_vfd_init, gu_vfd_deinit, NULL, &data##node_id, &config##node_id,  \
-			 POST_KERNEL, CONFIG_DISPLAY_INIT_PRIORITY, &gu_vfd_driver_api);        \
-
+			 POST_KERNEL, CONFIG_DISPLAY_INIT_PRIORITY, &gu_vfd_driver_api);
 
 DT_FOREACH_STATUS_OKAY(DT_DRV_COMPAT, GU_VFD_DEFINE)
--- samples/drivers/display_vfd/src/main.c
+++ samples/drivers/display_vfd/src/main.c
@@ -15,11 +15,20 @@ LOG_MODULE_REGISTER(vfd_display_sample, LOG_LEVEL_DBG);
 int main(void)
 {
 	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(vfd_display));
+	struct display_capabilities capabilities;
+
+
 
 	if (!device_is_ready(dev)) {
 		LOG_ERR("display device is not ready.");
 		return 0;
 	}
+
+	LOG_INF("Display sample for %s", dev->name);
+	display_get_capabilities(dev, &capabilities);
+
+	struct display_driver_api *api = (struct display_driver_api *)dev->api;
+	api->write(dev, 4, 0, 0, 0);
 
 	return 0;
 }
